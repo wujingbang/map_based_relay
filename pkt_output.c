226c38215f5a2b93cf47308b800f0542907b3c5b
wujingbang <wujingbang@gmail.com>
2017-02-23 21:18:23 +0800

    pkt_output

--- pkt_output.c
+++ pkt_output.c
@@ -54,15 +54,17 @@ int eth_header_dirty(struct sk_buff *skb, unsigned short type,
 unsigned int output_handler(const struct nf_hook_ops *ops, struct sk_buff *skb,
 		const struct net_device *in, const struct net_device *out, int (*okfn) (struct sk_buff *))
 {
-	int err;
+	int err, ret;
+	struct netdev_hw_addr *ha;
+	int i =0;
 	//mbr_dbg(debug_level, ANY, "")
-#define GEL01 {0x54,0x27,0x1e,0xa4,0xca,0xe3}
-#define GEL02 {0x54,0x27,0x1e,0x1a,0x77,0x99}
-#define TEST {0x11,0x22,0x33,0x44,0x55,0x66}
-#define ETH_P_MAPRELAY 0x6559
-	unsigned char	h_source[ETH_ALEN] = GEL02;
-	unsigned char	h_dest[ETH_ALEN] = GEL01;
-	unsigned char test[ETH_ALEN] = TEST;
+//#define GEL01 {0x54,0x27,0x1e,0xa4,0xca,0xe3}
+//#define GEL02 {0x54,0x27,0x1e,0x1a,0x77,0x99}
+//#define TEST {0x11,0x22,0x33,0x44,0x55,0x66}
+//#define ETH_P_MAPRELAY 0x6559
+	unsigned char	h_source[ETH_ALEN];// = GEL02;
+//	unsigned char	h_dest[ETH_ALEN] = GEL01;
+//	unsigned char test[ETH_ALEN] = TEST;
 
 	unsigned char relay_mac[ETH_ALEN];
 	unsigned char dst_mac[ETH_ALEN];
@@ -72,28 +74,28 @@ unsigned int output_handler(const struct nf_hook_ops *ops, struct sk_buff *skb,
 		return NF_ACCEPT;
 	}
 
-//	ret = mbr_forward( dst_mac, relay_mac, skb, global_graph);
-//	if(ret < 0) {
-//		mbr_dbg(debug_level, ANY, "mbr_forward failed!\n");
-//		return NF_ACCEPT;
-//	}
-	struct netdev_hw_addr *ha;
-	int i =0;
+	ret = mbr_forward( dst_mac, relay_mac, skb, global_graph);
+	if(ret < 0) {
+		mbr_dbg(debug_level, ANY, "mbr_forward failed!\n");
+		return NF_ACCEPT;
+	}
+
 	rcu_read_lock();
 	for_each_dev_addr(out, ha) {
-		mbr_dbg(debug_level, ANY, "num: %d, out_mac: %x:%x:%x:%x:%x:%x\n",
-				++i,
-				ha->addr[0], ha->addr[1], ha->addr[2],
-				ha->addr[3], ha->addr[4], ha->addr[5]);
+//		mbr_dbg(debug_level, ANY, "num: %d, out_mac: %x:%x:%x:%x:%x:%x\n",
+//				++i,
+//				ha->addr[0], ha->addr[1], ha->addr[2],
+//				ha->addr[3], ha->addr[4], ha->addr[5]);
+		memcpy(h_source, ha->addr, ETH_ALEN);
 	}
 	rcu_read_unlock();
 
 	__skb_pull(skb, skb_network_offset(skb));
 	//err = dev_hard_header(skb, skb->dev, ETH_P_IP, h_dest, h_source, skb->len);
-	err = eth_header_dirty(skb, ETH_P_MAPRELAY/*ETH_P_IP*/, h_dest, h_source, skb->len);
+	err = eth_header_dirty(skb, ETH_P_MAPRELAY/*ETH_P_IP*/, dst_mac, h_source, skb->len);
 	if (err < 0)
 		mbr_dbg(debug_level, ANY, "eth_header_dirty ERROR!! %d\n", err );
-	err = push_relay_mac(skb, test);
+	err = push_relay_mac(skb, relay_mac);
 	if (err < 0)
 		mbr_dbg(debug_level, ANY, "push_relay_mac ERROR!! %d\n", err );
 
